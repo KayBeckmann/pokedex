f8efdd6c5336f0bf5cdd9b0d88d4bda29d59c313
Kay Beckmann <info@beckmann-md.de>
2023-01-09 21:53:09 +0100

    Bug-Fix in Namen

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ async function loadApi(quantity){
       let url = `https://pokeapi.co/api/v2/pokemon/${querryPokemon}`;
       let pokemon = await fetch(url);
       pokemon = await pokemon.json();
-      console.log(pokemon);
       pokemonExtract(pokemon);
     }
   }
